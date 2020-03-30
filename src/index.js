14e3a0f53012555b32c79f8b993b80251fbd3524
Louis Harang <narah@pm.me>
2020-03-30 18:13:06 +0200

    feat: update with new filter format

--- src/index.js
+++ src/index.js
@@ -49,8 +49,6 @@ export default (apiUrl, userSettings = {}) => (type, resource, params) => {
         "itemsPerPage": perPage
       }
 
-      console.log(query);
-
       // Add all filter params to query.
       Object.keys(params.filter || {}).forEach((key) => {
         query[`${key}`] = params.filter[key]
@@ -123,7 +121,7 @@ export default (apiUrl, userSettings = {}) => (type, resource, params) => {
 
       // Add all filter params to query.
       Object.keys(params.filter || {}).forEach((key) => {
-        query[`filter[${key}]`] = params.filter[key]
+        query[`${key}[]`] = params.filter[key]
       })
 
 
